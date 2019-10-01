45383d6b4d7a2a201df39f82df3996b42dd68e1f
remi.odufuye <remi.odufuye@gmail.com>
2019-10-01 21:30:31 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'] 
+
+function addElementToBeginningOfArray(array ,element) {
+ var arrayNew = [element,...array] ; 
+ return arrayNew ; 
+} 
+
+function destructivelyAddElementToBeginningOfArray(array ,element) {
+  array.unshift(element) ;
+  return array ;
+}  
+
+function addElementToEndOfArray(array , element) {
+var arrayNew = [...array , element] ; 
+ return arrayNew ; 
+  
+}
+
+function destructivelyAddElementToEndOfArray(array , element) {
+  array.push(element) ;
+  return array ; 
+  
+}
+
+// Accessing An Element Exercises 
+
+
+
